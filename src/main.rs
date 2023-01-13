ac6899848a226e8cca98cb450694365c887eb6c9
Eliza Weisman <eliza@buoyant.io>
2023-01-13 09:37:36 -0800

    chore: clean stuff up a little bit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+// If using the `binstart` feature of `esp-idf-sys`, always keep this module
+// imported
+use esp_idf_sys as _;
 
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+fn main() -> anyhow::Result<()> {
+    // It is necessary to call this function once. Otherwise, some patches to the
+    // runtime implemented by esp-idf-sys might not link properly. See
+    // https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
     println!("Hello, world!");
+
+    Ok(())
 }
