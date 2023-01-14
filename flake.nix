701e3a0c7402bdc114ccdd3e54af7d8595acc810
Eliza Weisman <eliza@buoyant.io>
2023-01-14 11:05:40 -0800

    chore: add actually working nix flake (!!!)

---
+++ flake.nix
@@ -0,0 +1,49 @@
+# /!\ EXTREMELY SERIOUS WARNING /!\
+#
+# For some absurd reason, this will fail if a system-wide `clang` installation
+# exists. I don't know why this is.
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+  };
+
+  description = "flake for ESP32-c3 Rust development";
+
+  outputs = { self, flake-compat, nixpkgs, flake-utils, nixpkgs-esp-dev, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ nixpkgs-esp-dev.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShell = pkgs.mkShell rec {
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          buildInputs = with pkgs; [
+            # rust tools
+            rustup
+            rust-analyzer
+            cargo-generate
+            # rust ESP tools
+            cargo-espflash
+            cargo-espmonitor
+
+            # esp-idf-sys dependencies:
+            cmake
+            ninja
+            python3Packages.python
+            python3Packages.pip
+            python3Packages.virtualenv
+            (gcc-riscv32-esp32c3-elf-bin.override {
+              version = "2021r2-patch5";
+              hash = "sha256-99c+X54t8+psqOLJXWym0j1rOP0QHqXTAS88s81Z858=";
+            })
+          ];
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
