c9c0051caa47b20f9a25b4270b6a1ad90b1a23fd
Eliza Weisman <eliza@buoyant.io>
2023-02-01 09:27:19 -0800

    chore: rerun build if html changes

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=src/http/index.html");
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
     Ok(())
